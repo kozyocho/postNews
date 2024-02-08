a2dbd09bbc6599517f0dc42fe3cd78506b2bb9f8
yamasaki <shohei615615@gmail.com>
2024-02-08 13:23:36 +0800

    add ignore file and hide keys

--- postNews.js
+++ postNews.js
@@ -1,13 +1,15 @@
-const fetch = require('node-fetch');
-const axios = require('axios');
-const { IncomingWebhook } = require('@slack/webhook');
-const { Translate } = require('@google-cloud/translate').v2;
+const fetch = require("node-fetch");
+const axios = require("axios");
+const { IncomingWebhook } = require("@slack/webhook");
+const { Translate } = require("@google-cloud/translate").v2;
+require("dotenv").config();
 
 // APIキーとSlackの設定
-const slackWebhookUrl = "https://hooks.slack.com/services/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";
-const slackChannel = "C05L1M7JC8L"; // 投稿するSlackのチャンネルを指定
-const googleApiKey = 'AIzaSyD4HQ7xyXcmLCFkcpYo8ve-R6fxSB1H1HY';
-const newsdataiokey = 'pub_2718615567c2ba610970ca8b24acdb7eba883';
+const slackWebhookUrl =
+process.env.slackWebhookUrl;
+const slackChannel = process.env.slackChannel; // 投稿するSlackのチャンネルを指定
+const googleApiKey = process.env.googleApiKey;
+const newsdataiokey = process.env.newsdataiokey;
 
 var countArticle = 0;
 
@@ -20,21 +22,28 @@ function truncateString(str, maxLength) {
   if (str.maxLength <= maxLength) {
     return str;
   } else {
-    return str.slice(0, maxLength - 3) + '...';
+    return str.slice(0, maxLength - 3) + "...";
   }
 }
 
 //引数で受け取った文字列を特定の言語に翻訳
 async function translateText(str, sourceLang, targetLang) {
-  const url = `https://translation.googleapis.com/language/translate/v2?key=${googleApiKey}&q=${encodeURIComponent(str)}&source=${sourceLang}&target=${targetLang}`;
+  const url = `https://translation.googleapis.com/language/translate/v2?key=${googleApiKey}&q=${encodeURIComponent(
+    str
+  )}&source=${sourceLang}&target=${targetLang}`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    if (data && data.data && data.data.translations && data.data.translations.length > 0) {
+    if (
+      data &&
+      data.data &&
+      data.data.translations &&
+      data.data.translations.length > 0
+    ) {
       return data.data.translations[0].translatedText;
     } else {
-      throw new Error('Error: Failed to translate the text');
+      throw new Error("Error: Failed to translate the text");
     }
   } catch (error) {
     throw new Error(`Error: ${error.message}`);
@@ -55,19 +64,27 @@ async function getAndPostNews() {
       const link = result.link;
 
       // タイトルと説明を翻訳
-      const translatedTitle = await translateText(title, sourceLanguage, targetLanguage);
-      const translatedDescription = await translateText(description, sourceLanguage, targetLanguage);
+      const translatedTitle = await translateText(
+        title,
+        sourceLanguage,
+        targetLanguage
+      );
+      const translatedDescription = await translateText(
+        description,
+        sourceLanguage,
+        targetLanguage
+      );
 
       // Slackに投稿
       await slack.send({
         channel: slackChannel,
-        text: `*${translatedTitle}*\n${translatedDescription}\n${link}`
+        text: `*${translatedTitle}*\n${translatedDescription}\n${link}`,
       });
 
       countArticle += 1;
     }
 
-    console.log(countArticle + '件の記事をSlackに投稿しました。');
+    console.log(countArticle + "件の記事をSlackに投稿しました。");
   } catch (error) {
     console.error("エラー:", error.message);
     console.log("Slackの投稿に失敗しました。");
